c4075b1ef785aefbdf1cc006bf94e3ddeadacc16
Pugli87 <119467014+Pugli87@users.noreply.github.com>
2023-07-27 15:58:19 -0500

    styled component y propTypes

--- src/components/feedback/section/Section.jsx
+++ src/components/feedback/section/Section.jsx
@@ -1,12 +1,42 @@
-import React from "react";
+import React, { Component } from 'react';
+import styled from 'styled-components';
+import PropTypes from 'prop-types';
 
+const SectionCont = styled.div``;
+
+const FeedbackTitle = styled.h2`
+  font-weight: 500;
+  &::first-letter {
+    text-transform: uppercase;
+  }
+`;
+/*
 const Section = ({ title, children }) => {
   return (
-    <div>
-      <h2>{title}</h2>
+    <SectionCont>
+      <FeedbackTitle>{title}</FeedbackTitle>
       {children}
-    </div>
+    </SectionCont>
   );
 };
 
 export default Section;
+*/
+
+export default class section extends Component {
+  static propTypes = {
+    title: PropTypes.string.isRequired,
+    children: PropTypes.node.isRequired,
+  };
+
+  render() {
+    const { title, children } = this.props;
+
+    return (
+      <SectionCont>
+        <FeedbackTitle>{title}</FeedbackTitle>
+        {children}
+      </SectionCont>
+    );
+  }
+}
